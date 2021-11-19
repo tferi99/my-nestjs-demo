4b29ab9ac23a7b5491c3503f2ef72f90be10cff1
ftoth <toth_ferenc@hotmail.com>
2021-11-19 07:07:42 +0100

    .

--- src/deco/decorators/request-param.decorator.ts
+++ src/deco/decorators/request-param.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { DECO_PREFIX } from './prefix';
+import { DECO_PREFIX } from './deco-constants';
 
-export const ParamDeco1 = createParamDecorator((data: string, ctx: ExecutionContext) => {
+export const RequestParamDeco = createParamDecorator((data: string, ctx: ExecutionContext) => {
   console.log(DECO_PREFIX + 'ParamDeco1: called', data);
   const request = ctx.switchToHttp().getRequest();
   return request.params[data];
