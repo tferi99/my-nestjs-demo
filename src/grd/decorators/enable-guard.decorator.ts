15d0b8c0be7c5790aaf6b076296ac7f42ea91c7e
ftoth <toth_ferenc@hotmail.com>
2021-12-10 00:50:49 +0100

    .

--- src/grd/decorators/enable-guard.decorator.ts
+++ src/grd/decorators/enable-guard.decorator.ts
@@ -1,6 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const ENABLE_GUARD_KEY = 'G2_grd_kill';
+export const ENABLE_GUARD_KEY = 'EnableGuard';
+export const ENABLE_GUARD_CONFIGS_KEY = 'EnableGuardConfigs';
 /*export function EnableGuard(enabled: boolean) {
   return (...args : any[]) => {
     SetMetadata(ENABLE_GUARD_KEY, enabled);
@@ -28,8 +29,28 @@ export interface GuardConfig {
   enabled: boolean;
 }
 
-export const EnableGuard = (guardConfig: GuardConfig) => {
+/*export const EnableGuard = (guardConfig: GuardConfig) => {
   console.log('EnableGuard()', guardConfig);
   return SetMetadata(ENABLE_GUARD_KEY, guardConfig.enabled);
-}
+}*/
 
+/*
+export const EnableGuard = (guardConfig: GuardConfig) => {
+  const factory = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    Reflect.defineMetadata(ENABLE_GUARD_KEY, guardConfig.enabled, descriptor.value);
+  }
+  return factory;
+}
+*/
+export const EnableGuard = (guardConfig: GuardConfig) => {
+  const factory = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    let configs: Map<Global1GuardTarget | string, GuardConfig> = Reflect.getMetadata(ENABLE_GUARD_CONFIGS_KEY, target, propertyKey);
+    if (configs === undefined) {
+      configs = new Map<Global1GuardTarget | string, GuardConfig>();
+      Reflect.defineMetadata(ENABLE_GUARD_CONFIGS_KEY, configs, descriptor.value);
+    }
+    configs.set(guardConfig.target, guardConfig);
+  }
+  return factory;
+}
+let result = Reflect.getMetadata(metadataKey, target, propertyKey);
